85afe607573890a71f7fd46800df709567516c62
Jin Zhao <zjaml2007@gmail.com>
2016-09-05 23:19:18 +0900

    c

--- app/components/Store.js
+++ app/components/Store.js
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
 import {View, Text,
-  StyleSheet} from 'react-native'
+  StyleSheet, Navigator} from 'react-native'
 
 class Store extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={{
-    margin: 109
         }}>Hello</Text>
       </View>
     )
   }
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    paddingTop: 64
   }
 })
 export default Store;
