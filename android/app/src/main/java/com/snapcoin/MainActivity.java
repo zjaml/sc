506c3d36cb22701924cc84cc82e1966ccee3a8f3
Jin Zhao <JZhao@tradestation.com>
2016-09-06 15:42:45 +0900

    rnpm

--- android/app/src/main/java/com/snapcoin/MainActivity.java
+++ android/app/src/main/java/com/snapcoin/MainActivity.java
@@ -1,6 +1,10 @@
 package com.snapcoin;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 
 public class MainActivity extends ReactActivity {
 
