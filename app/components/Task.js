feb33bd50d1613e4175917b82c5bada01f781b86
Jin Zhao <JZhao@tradestation.com>
2016-09-06 15:36:26 +0900

    add dependencies

--- app/components/Task.js
+++ app/components/Task.js
@@ -2,19 +2,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import {View, Text, StyleSheet} from 'react-native'
 import globalStyles from '../globalStyle'
+import {Button} from 'react-native-material-design'
 
 class Task extends Component {
   render() {
     return (
       <View style={globalStyles.navContentContainer}>
         <Text style={styles.title}>{this.props.task.description}</Text>
+        <View style={{ alignSelf: 'flex-end', paddingRight: 20 }}>
+          <Button text="Take this task" primary></Button>
+        </View>
       </View>
     )
   }
 }
 
-function mapStateToProps(state,{id}){
-  return {task: state.entities.tasks[id]}
+function mapStateToProps(state, {id}) {
+  return { task: state.entities.tasks[id] }
 }
 
 var styles = StyleSheet.create({
